6bd6c2e47a32752cf67cc069bc3f98069ae9d6eb
nik-kotine <mswv280407@gmail.com>
2025-05-06 19:13:04 -0500

    fix: updated dependencies

--- src/main/java/hackathon_1/UserLimits/domain/UserLimits.java
+++ src/main/java/hackathon_1/UserLimits/domain/UserLimits.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserLimits {
+public class    UserLimits {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
